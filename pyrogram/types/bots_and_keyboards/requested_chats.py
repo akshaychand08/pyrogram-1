de9035491d2c6c152e055f8e4eb9009f4a630797
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-01-05 13:55:48 +0530

    Update requested_chats.py

--- pyrogram/types/bots_and_keyboards/requested_chats.py
+++ pyrogram/types/bots_and_keyboards/requested_chats.py
@@ -1,87 +1,77 @@
-#  Pyrogram - Telegram MTProto API Client Library for Python
-#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#  Pyrofork - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2022-present Mayuri-Chan <https://github.com/Mayuri-Chan>
 #
-#  This file is part of Pyrogram.
+#  This file is part of Pyrofork.
 #
-#  Pyrogram is free software: you can redistribute it and/or modify
+#  Pyrofork is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published
 #  by the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  Pyrogram is distributed in the hope that it will be useful,
+#  Pyrofork is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public License
-#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
-
-from typing import List, Union
-
-import pyrogram
-from pyrogram import enums
-from pyrogram import raw, utils, types
+#  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
 
 from ..object import Object
-
+from pyrogram import enums, raw, types
+from typing import Union, List
 
 class RequestedChats(Object):
     """Contains information about requested chats.
 
     Parameters:
         button_id (``int``):
-            Identifier of button.
+            Button identifier.
 
-        chats (List of :obj:`~pyrogram.types.Chat`):
-            List of requested chats.
+        chats (List of :obj:`~pyrogram.types.RequestedChat`, *optional*):
+            List of chats.
+
+        users (List of :obj:`~pyrogram.types.RequestedUser` *optional*):
+            List of users.
     """
-
     def __init__(
-        self, *,
-        client: "pyrogram.Client" = None,
+        self,
         button_id: int,
-        chats: List["types.Chat"],
+        chats: List["types.RequestedChat"] = None,
+        users: List["types.RequestedUser"] = None
     ):
-        super().__init__(client)
+        super().__init__()
 
         self.button_id = button_id
         self.chats = chats
+        self.users = users
 
     @staticmethod
-    def _parse(
+    async def _parse(
         client,
-        action: Union[
+        request: Union[
             "raw.types.MessageActionRequestedPeer",
             "raw.types.MessageActionRequestedPeerSentMe"
         ]
     ) -> "RequestedChats":
-        _requested_chats = []
-
-        for requested_peer in action.peers:
-            peer_id = utils.get_peer_id(requested_peer)
-            peer_type = utils.get_peer_type(peer_id)
-
-            if peer_type == "user":
-                chat_type = enums.ChatType.PRIVATE
-            elif peer_type == "chat":
-                chat_type = enums.ChatType.GROUP
-            else:
-                chat_type = enums.ChatType.CHANNEL
-
-            _requested_chats.append(
-                types.Chat(
-                    id=peer_id,
-                    type=chat_type,
-                    first_name=getattr(requested_peer, "first_name", None),
-                    last_name=getattr(requested_peer, "last_name", None),
-                    username=getattr(requested_peer, "username", None),
-                    photo=types.ChatPhoto._parse(client, getattr(requested_peer, "photo", None), peer_id, 0),
-                    client=client
-                )
-            )
+        button_id = request.button_id
+        chats = []
+        users = []
+        for chat in request.peers:
+            if (
+                isinstance(chat, raw.types.RequestedPeerChat)
+                or isinstance(chat, raw.types.RequestedPeerChannel)
+                or isinstance(chat, raw.types.PeerChat)
+                or isinstance(chat, raw.types.PeerChannel)
+            ):
+                chats.append(await types.RequestedChat._parse(client, chat))
+            elif (
+                isinstance(chat, raw.types.RequestedPeerUser)
+                or isinstance(chat, raw.types.PeerUser)
+            ):
+                users.append(await types.RequestedUser._parse(client, chat))
 
         return RequestedChats(
-            button_id=action.button_id,
-            chats=types.List(_requested_chats),
-            client=client
+            button_id,
+            chats if len(chats) > 0 else None,
+            users if len(users) > 0 else None
         )
